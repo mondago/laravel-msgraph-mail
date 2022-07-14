8c946d65f305005d9683fcf63e298aa8c3fb9780
Yanis <yanis.kross@gmail.com>
2022-07-14 16:25:06 +0100

    Fix boot method scope (#5) (#6)

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 
 class ServiceProvider extends LaravelServiceProvider
 {
-    protected function boot()
+    public function boot()
     {
         $this->app->get('mail.manager')->extend('graph-api', function (array $config) {
             return new Transport(new Client(), $config);
